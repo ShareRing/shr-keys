de9d62149ffd320f3953d8e8df20aac6f8222a00
Trang Tran <trang.tran@sharering.network>
2018-06-15 16:56:28 +0700

    ADD: encrypt/decrypt keypair; UPDATE: comments for encrypt and encode functions

--- src/encode.js
+++ src/encode.js
@@ -7,22 +7,46 @@ const Buffer = require('buffer').Buffer;
 
 
 
-
+/**
+ * calculate *checksum* of payload. 
+ * Checksum = first 4 bytes of sha3_256
+ * @param {array} payload - data to be checked
+ * @return {Buffer} first 4 bytes of hash value of payload, using sha3_256
+ */
 function checksum (payload) {
   return new Buffer(sha3_256(Buffer.concat(payload)), 'hex').slice(0, 4)
 }
 
+
+/**
+ * encode Ethereum address and network to ShareLEedger address
+ * ShareLedger address: base58 encode of (networkID + Ethereum address + checksum)
+ *
+ * @param {string} network - hex value of network, prefixed by '0x'
+ * @param {string} address - hex value of Ethereum address, prefixed by '0x'
+ * @return ShareLedger address
+ */
 function encode ({network, address}) {
   const payload = [hex.decode(network.slice(2)), new Buffer(address.slice(2), 'hex')]
+
   payload.push(checksum(payload))
+
   return base58.encode(Buffer.concat(payload))
 }
 
+
+/**
+ * decode ShareLedger address to network and Ethereum address
+ * @param {string} encoded - base58 encoding of ShareLedger address
+ * @return {object} {network, address}
+ */
 function decode (encoded) {
   const data = Buffer.from(base58.decode(encoded))
   const netLength = data.length - 24
+
   const network = data.slice(0, netLength)
   const address = data.slice(netLength, 20 + netLength)
+
   const check = data.slice(netLength + 20)
   if (check.equals(checksum([network, address]))) {
     return {
@@ -30,19 +54,11 @@ function decode (encoded) {
       address: `0x${address.toString('hex')}`
     }
   } else {
+
     throw new Error('Invalid address checksum')
   }
 }
 
-
-//function isMNID (encoded) {
-  //try {
-    //const data = Buffer.from(base58.decode(encoded))
-    //return data.length > 24 && data[0] === 1
-  //} catch (e) {
-    //return false
-  //}
-//}
 
 
 module.exports = { checksum, encode, decode};
---
+++ src/encrypt.js
@@ -0,0 +1,72 @@
+const elliptic = require("elliptic");
+const secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
+const pbkdf2 = require('pbkdf2');
+const aesjs = require('aes-js');
+
+KEY_LENGTH = 256; // Length of secret key. Can be 128, 192, or 256
+
+/**
+ * encrypt *plaintext* using *secretKey*, supposedly generated uisng *generateSecretKey* function
+ * @param {string} plaintext - text to be encrypted
+ * @param {array} secretKey - key used to encrypt plaintext
+ * @return {string} hex encoded ciphertext
+ */
+const encrypt = (plaintext, secretKey) => {
+    var textBytes = aesjs.utils.utf8.toBytes(plaintext);
+
+    var aesCtr = new aesjs.ModeOfOperation.ctr(secretKey, new aesjs.Counter(5));
+    
+    var encryptedBytes = aesCtr.encrypt(textBytes);
+    
+    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+
+    return encryptedHex;
+}
+
+
+/**
+ * decrypt *ciphertext* using *secretKey*, supposedly generated using *generateSecretKey* function
+ * @param {string} ciphertext - hex encoded ciphertext to be decrypted
+ * @param {array} secretKey - key used to decrypt ciphertext
+ * @return {string} plaintext
+ */
+const decrypt = (ciphertext, secretKey) => {
+    var byteData = aesjs.utils.hex.toBytes(ciphertext);
+
+    var aesCtr = new aesjs.ModeOfOperation.ctr(secretKey, new aesjs.Counter(5));
+
+    var decryptedBytes = aesCtr.decrypt(byteData);
+
+    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+
+    return decryptedText;
+}
+
+/**
+ * generate secret key
+ * @param {string} password - user input password
+ * @param {string} salt - system salt
+ * @return {Buffer} - array of integer, each integer represent a byte. Array length = KEY_LENGTH / 8
+ */
+const generateSecretKey = (password, salt) => {
+    return pbkdf2.pbkdf2Sync(password, salt, 1, KEY_LENGTH/8, 'sha512')
+}
+
+
+module.exports = { encrypt, decrypt, generateSecretKey };
+
+
+if ( require.main == module ){
+    console.log("Generate key...");
+    key = generateSecretKey("ShareRingiscaring", "220NguyenDinhChieu");
+    console.log(key);
+    
+    plaintext = "ShareRing";
+
+    ciphertext = encrypt(plaintext, key);
+    console.log("Encrypted data: ", ciphertext);
+
+
+    result = decrypt(ciphertext, key);
+    console.log("Decrypted data:", result);
+}
