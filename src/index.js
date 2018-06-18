1c3f9895eee6e484263b67105267523ea67029d2
Tung Tran <tung.tran@sharering.network>
2018-06-18 11:25:27 +0700

    add main file

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import Address from 'address';
+import Encode from 'encode';
+import Encrypt from 'encrypt';
+
+
+export default {
+    createAccount: function (entropy) {
+        return Address.create(entropy);
+    },
+    encrypt: function (plaintext, secretKey) {
+        if(plaintext && secretKey)
+        {
+            return Encrypt.encrypt(plaintext, secretKey)
+        }else {
+            return '';
+        }
+
+    },
+    generateSecretKey: function (password, salt) {
+        if(password && salt)
+        {
+            return Encrypt.generateSecretKey(password, salt)
+        }else {
+            return '';
+        }
+
+    },
+    decrypt: function (ciphertext, secretKey) {
+        if(ciphertext && secretKey)
+        {
+            return Encrypt.decrypt(ciphertext, secretKey)
+        }else {
+            return '';
+        }
+
+    },
+    encode: function ({network, address}) {
+        if(network && address)
+        {
+            return Encode.encode({network, address})
+        }else {
+            return '';
+        }
+
+    },
+    decode: function (encoded) {
+        if(encoded)
+        {
+            return Encode.decode(encoded)
+        }else {
+            return '';
+        }
+
+    },
+
+}
